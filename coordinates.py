74eedc0b3de5d6f600863fd53d71fa37c6119516
theGoldenKiwi2 <123649011+theGoldenKiwi2@users.noreply.github.com>
2024-06-27 11:03:02 +0200

    Add files via upload

--- coordinates.py
+++ coordinates.py
@@ -1,11 +1,12 @@
 import cv2
 
+
 # define a function to display the coordinates of
 # the points clicked on the image
 
 def click_event(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
-        print(f'({x},{y})')
+        print(f'{x},{y}')
 
         # put coordinates as text on the image
         cv2.putText(img, f'({x},{y})', (x, y),
@@ -16,18 +17,13 @@ def click_event(event, x, y, flags, params):
 
 
 # read the input image
-img = cv2.imread('CO_023.jpg')
-
+img = cv2.imread('CO_020.jpg')
 # create a window
 cv2.namedWindow('Point Coordinates')
-
 # bind the callback function to window
 cv2.setMouseCallback('Point Coordinates', click_event)
 
-# display the image
-while True:
-    cv2.imshow('Point Coordinates', img)
-    k = cv2.waitKey(1) & 0xFF
-    if k == 27:
-        break
+cv2.imshow('Point Coordinates', img)
+cv2.waitKey(0)
+
 cv2.destroyAllWindows()
